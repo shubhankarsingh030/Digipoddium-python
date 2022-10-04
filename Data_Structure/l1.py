58effeda8639a6b462082616a611de1684b4efcb
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2022-10-04 16:57:25 +0530

    List

---
+++ Data_Structure/l1.py
@@ -0,0 +1,36 @@
+
+Elements = ["Hydrogen","Helium","lithium"]
+
+print(Elements)
+print('Individual elements:')
+print(Elements[0])
+print(Elements[1])
+print(Elements[2])
+
+#changing a list element(mutability)
+
+Elements[1] = 'Carbon'
+print(Elements)
+
+
+#defining list in python-
+
+#List of string
+list1 = ["raju","kamala","rishab","virat"]
+print(list1)
+
+#list of no.
+
+list2 = [1, 2, 3, 4, 5, 6, 7, 8]
+print(list2)
+#mix datalist
+
+list3 = ['ravi', 90, 60, 66, 54, 45, 56, "b",False , 7.554]
+print(list3)
+
+
+#nested list
+
+list4 = [[1,2,3],[3,4,5][2,1]]
+
+print(list4)
